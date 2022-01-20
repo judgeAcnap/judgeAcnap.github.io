3a2eb76810b858d9e4b3f47592a1943d4e3d08cf
judgeancap <kekw@fuck.you>
2022-01-20 06:16:36 +0300

    kekw

--- html/src/io/github/judgeacnap/client/HtmlLauncher.java
+++ html/src/io/github/judgeacnap/client/HtmlLauncher.java
@@ -1,8 +1,13 @@
 package io.github.judgeacnap.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.GwtGraphics;
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.Window;
 import io.github.judgeacnap.JudgeAcnapGame;
 
 public class HtmlLauncher extends GwtApplication {
@@ -10,9 +15,9 @@ public class HtmlLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
                 // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
+                //return new GwtApplicationConfiguration(true);
                 // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(Window.getClientWidth()/2, 320, false);
         }
 
         @Override
