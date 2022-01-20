5ec4b5b8ffa3e2decd0045009df22d674340578b
judgeancap <kekw@fuck.you>
2022-01-20 21:05:39 +0300

    resize fix

--- html/src/io/github/judgeacnap/client/HtmlLauncher.java
+++ html/src/io/github/judgeacnap/client/HtmlLauncher.java
@@ -10,42 +10,30 @@ import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
 import io.github.judgeacnap.JudgeAcnapGame;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-class MyResizeHandler implements ResizeHandler{
-    GwtApplication GwtApplication;
-
-    public MyResizeHandler(GwtApplication gwtApp){
-        GwtApplication = gwtApp;
-    }
-
-    @Override
-    public void onResize(ResizeEvent ev) {
-        if (Gdx.graphics.isFullscreen()) {
-            Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
-        } else {
-            int width = ev.getWidth(), height = ev.getHeight();
-            if (ev.getWidth() >= ev.getHeight()) {
-                width = height * 2;
-            } else {
-                height = (int)(ev.getWidth() * 0.5);
-            }
-            GwtApplication.getRootPanel().setWidth("" + width + "px");
-            GwtApplication.getRootPanel().setHeight("" + height + "px");
-            GwtApplication.getApplicationListener().resize(width, height);
-            Gdx.graphics.setWindowedMode(width, height);
-            //GwtApplication.log("ssssss: ", width + "  " + height);
-        }
-    }
-}
-
 public class HtmlLauncher extends GwtApplication {
-    private MyResizeHandler MyResizeHandler = new MyResizeHandler(this);
     @Override
     public void onModuleLoad () {
         super.onModuleLoad();
-        Window.addResizeHandler(MyResizeHandler);
+        Window.addResizeHandler(new ResizeHandler() {
+            @Override
+            public void onResize(ResizeEvent ev) {
+                if (Gdx.graphics.isFullscreen()) {
+                    Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                } else {
+                    int width = ev.getWidth(), height = ev.getHeight();
+                    if (ev.getWidth() >= ev.getHeight()) {
+                        width = height * 2;
+                    } else {
+                        height = (int)(ev.getWidth() * 0.5);
+                    }
+                    getRootPanel().setWidth("" + width + "px");
+                    getRootPanel().setHeight("" + height + "px");
+                    getApplicationListener().resize(width, height);
+                    Gdx.graphics.setWindowedMode(width, height);
+                    log("ssssss", width + "  " + height);
+                }
+            }
+        });
     }
 
     @Override
@@ -57,14 +45,14 @@ public class HtmlLauncher extends GwtApplication {
             height = (int) (width * 0.5);
         }
         GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(width, height, true);
-        //cfg.fullscreenOrientation = GwtGraphics.OrientationLockType.LANDSCAPE;
+        cfg.fullscreenOrientation = GwtGraphics.OrientationLockType.LANDSCAPE;
         return cfg;
     }
 
     @Override
     public ApplicationListener createApplicationListener() {
         Window.enableScrolling(false);
-        //this.setLogLevel(LOG_INFO);
+        this.setLogLevel(LOG_INFO);
         return new JudgeAcnapGame();
     }
 }
